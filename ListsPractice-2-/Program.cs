5349a4cd386e31623ef40d127cdbcbc60760117a
auro <auro@DESKTOP-AV8I9BT>
2024-08-24 04:19:37 +0300

    Add project files.

---
+++ ListsPractice-2-/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hoşgeldiniz almak istediğiniz kahveleri yazar mısınız?");
+
+        List<string> kahveListesi = new List<string>();
+        for (int i = 1; i < 6; i++)
+        {
+            Console.Write($"{i}. Kahve: ");
+            string girilenKahve = Console.ReadLine();
+            kahveListesi.Add(girilenKahve);
+        }
+        Console.WriteLine("\nGirdiğiniz kahveler\n-------------------\n");
+        foreach (string kahve in kahveListesi)
+        {
+
+            Console.WriteLine(kahve);
+        }
+    }
+
+}
+
